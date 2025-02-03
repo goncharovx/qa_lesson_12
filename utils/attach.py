c6b018dd2320bd0b5063a63ea49e0d443fde933f
Goncharov Anton <a.goncharrov@icloud.com>
2025-02-03 11:15:32 +0300

    upd attach logs for firefox

--- utils/attach.py
+++ utils/attach.py
@@ -7,8 +7,14 @@ def add_screenshot(browser):
     allure.attach(body=png, name='screenshot', attachment_type=AttachmentType.PNG, extension='.png')
 
 
-def add_logs(browser):
-    log = "".join(f'{text}\n' for text in browser.driver.get_log(log_type='browser'))
+def add_logs(browser, browser_type):
+    if browser_type == "firefox":
+        log = "Логи браузера недоступны для Firefox через WebDriver API."
+    else:
+        try:
+            log = "".join(f'{text}\n' for text in browser.driver.get_log(log_type='browser'))
+        except Exception:
+            log = "Ошибка при получении логов браузера."
     allure.attach(log, 'browser_logs', AttachmentType.TEXT, '.log')
 
 
@@ -18,7 +24,6 @@ def add_html(browser):
 
 
 def add_video(browser):
-    video_url = "https://selenoid.autotests.cloud/video/" + browser.driver.session_id + ".mp4"
-    html = "<html><body><video width='100%' height='100%' controls autoplay><source src='" \
-           + video_url + "' type='video/mp4'></video></body></html>"
-    allure.attach(html, 'video_' + browser.driver.session_id, AttachmentType.HTML, '.html')
+    video_url = f"https://selenoid.autotests.cloud/video/{browser.driver.session_id}.mp4"
+    html = f"<html><body><video width='100%' height='100%'controls autoplay><source src='{video_url}' type='video/mp4'></video></body></html>"
+    allure.attach(html, f'video_{browser.driver.session_id}', AttachmentType.HTML, '.html')
