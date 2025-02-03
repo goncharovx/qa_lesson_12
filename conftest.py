474625deda897b4d8a23a3121934a2a13023dfc7
Goncharov Anton <a.goncharrov@icloud.com>
2025-02-03 11:12:55 +0300

    upd settings for browser tyoe

--- conftest.py
+++ conftest.py
@@ -4,71 +4,77 @@ import pytest
 from dotenv import load_dotenv
 from selene import browser
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 from utils import attach
 
-login = os.getenv('LOGIN')
-password = os.getenv('PASSWORD')
+load_dotenv()
+
 selenoid_url = os.getenv('SELENOID_URL')
-DEFAULT_BROWSER_VERSION = "125.0"
+user = os.getenv('LOGIN')
+password = os.getenv('PASSWORD')
 
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_version', default='125.0')
-    parser.addoption('--browser_type')
+    parser.addoption("--browser_type", action="store", default="chrome", help="Тип браузера (chrome или firefox)")
+    parser.addoption("--browser_version", action="store", default="latest", help="Версия браузера")
 
 
-@pytest.fixture(scope="session", autouse=True)
-def load_env():
-    load_dotenv()
-    assert os.getenv("SELENOID_URL"), " Ошибка: Переменная окружения SELENOID_URL не задана!"
-    assert os.getenv("LOGIN"), " Ошибка: Переменная окружения LOGIN не задана!"
-    assert os.getenv("PASSWORD"), " Ошибка: Переменная окружения PASSWORD не задана!"
-
-
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
 def setup_browser(request):
-    browser_version = request.config.getoption('--browser_version')
-    browser_type = request.config.getoption('--browser_type')
-    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
+    browser_type = request.config.getoption("--browser_type").lower()
+    browser_version = request.config.getoption("--browser_version")
 
-    options = Options()
-    selenoid_capabilities = {
-        "browserName": browser_type,
-        "browserVersion": browser_version,
-        "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": True,
-            "setSize": (1280, 1024)
+    command_executor_url = f"https://{user}:{password}@{selenoid_url}/wd/hub"
+    print(f"Command Executor URL: {command_executor_url}")
+
+    if browser_type == 'chrome':
+        options = ChromeOptions()
+        options.add_argument("--ignore-certificate-errors")
+        capabilities = {
+            "browserName": "chrome",
+            "browserVersion": browser_version,
+            "selenoid:options": {
+                "enableVNC": True,
+                "enableVideo": True,
+                "screenResolution": "1280x1024x24"
+            }
         }
-    }
+        options.set_capability("selenoid:options", capabilities)
 
-    options.capabilities.update(selenoid_capabilities)
+    elif browser_type == 'firefox':
+        options = FirefoxOptions()
+        options.set_preference("security.insecure_field_warning.contextual.enabled", False)
+        capabilities = {
+            "browserName": "firefox",
+            "browserVersion": browser_version,
+            "selenoid:options": {
+                "enableVNC": True,
+                "enableVideo": True,
+                "screenResolution": "1280x1024x24"
+            }
+        }
+        options.set_capability("selenoid:options", capabilities)
+
+    else:
+        raise ValueError(f"Unsupported browser type: {browser_type}")
 
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=command_executor_url,
         options=options,
         keep_alive=True
     )
 
     browser.config.driver = driver
-
-    driver_options = webdriver.ChromeOptions()
-
-    driver_options.page_load_strategy = 'eager'
-    driver_options.add_argument('--ignore-certificate-errors')
-    browser.config.driver_options = driver_options
-
     browser.config.window_width = 1280
     browser.config.window_height = 724
-
     browser.config.base_url = 'https://demoqa.com'
 
     yield browser
 
-    attach.add_html(browser)
+    attach.add_logs(browser, request.config.getoption("--browser_type"))
     attach.add_screenshot(browser)
-    attach.add_logs(browser)
+    attach.add_html(browser)
     attach.add_video(browser)
     browser.quit()
